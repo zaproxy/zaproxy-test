19df5dda9579c5b34b95e67e8a4adb20c710bf7d
bjoern.kimminich@gmx.de <bjoern.kimminich@gmx.de@96ee5d4c-856f-cc55-8395-2371f7530af4>
2013-11-28 17:14:12 +0000

    implemented uncompressing error test case prepared additional test case

--- src/org/zaproxy/zap/extension/api/ApiResponseConversionUtilsUnitTest.java
+++ src/org/zaproxy/zap/extension/api/ApiResponseConversionUtilsUnitTest.java
@@ -1,6 +1,7 @@
 package org.zaproxy.zap.extension.api;
 
 import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.parosproxy.paros.network.HttpHeader;
 import org.parosproxy.paros.network.HttpMessage;
 import org.parosproxy.paros.network.HttpRequestHeader;
 import org.parosproxy.paros.network.HttpResponseHeader;
@@ -43,8 +45,16 @@ public class ApiResponseConversionUtilsUnitTest {
 	}
 	
 	@Test
+	public void nameOfApiResponseShouldBeConstant() {
+		ApiResponseSet response = ApiResponseConversionUtils.httpMessageToSet(0, message);
+
+		assertThat(response.getName(), is("message"));
+	}
+
+	@Test
 	public void historyIdShouldBecomeIdOfApiResponse() {
 		ApiResponseSet response = ApiResponseConversionUtils.httpMessageToSet(42, message);
+
 		assertThat(response.getValues(), hasEntry("id", "42"));
 	}
 
@@ -59,8 +69,13 @@ public class ApiResponseConversionUtilsUnitTest {
 	}
 
 	@Test
-	@Ignore
 	public void brokenCompressedResponseBodyShouldBeStoredAsStringRepresentationInApiResponse() {
+		given(responseHeader.getHeader(HttpHeader.CONTENT_ENCODING)).willReturn(HttpHeader.GZIP);
+		given(responseBody.getBytes()).willReturn(new byte[] {0,0,0});
+
+		ApiResponseSet response = ApiResponseConversionUtils.httpMessageToSet(0, message);
+		
+		assertThat(response.getValues(), hasEntry("responseBody", responseBody.toString()));
 	}
 
 }
